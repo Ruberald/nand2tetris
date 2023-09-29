7dabd2d4e3062570f6d3968b5f9f19d6147d24e3
Ruberald <sajidhossain@gmail.com>
2023-09-29 13:03:05 +0530

    Project 4 completed

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,58 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// n = 8192
+(START)
+    @8192
+    D = A
+    @n
+    M = D
+
+    // i = 0
+    @i
+    M = 0
+
+(LOOP)
+    //if i == n goto end
+    @i
+    D = M
+    @n
+    D = D - M
+    @END
+    D;JEQ
+
+    //RAM[SCREEN + i] to -1
+    @KBD
+    D = M
+    @WHITE
+    D;JEQ
+
+(BLACK)
+    @i
+    D = M
+    @SCREEN
+    A = A + D
+    M = -1
+    @INCREMENT
+    0;JMP
+
+(WHITE)
+    @i
+    D = M
+    @SCREEN
+    A = A + D
+    M = 0
+
+(INCREMENT)
+    // i = i + 1
+    @i
+    M = M + 1
+
+    //jump to LOOP
+    @LOOP
+    0;JMP
+
+(END)
+    @START
+    0;JMP
